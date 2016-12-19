4ae48368d4e2440791a54426b158b3abea0cef2f
Samantha Guergenenov <samguergen@gmail.com>
2016-12-19 14:09:06 -0500

    home controller scaff

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :furnitures
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
