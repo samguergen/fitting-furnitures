c46e85038de550b5cff55af4bae3e3960b46a755
Samantha Guergenenov <samguergen@gmail.com>
2016-12-29 11:36:14 -0500

    posing with premade forms

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+	has_secure_password
 	belongs_to :user
 	belongs_to :post
 	belongs_to :furniture
--- app/models/furniture.rb
+++ app/models/furniture.rb
@@ -1,4 +1,5 @@
 class Furniture < ActiveRecord::Base
+	has_secure_password
 	belongs_to :user
 	has_many :comments
 end
