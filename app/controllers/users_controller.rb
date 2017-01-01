83be857a04989f312dbd0e3c2bd8914acb620475
Samantha Guergenenov <samguergen@gmail.com>
2017-01-01 13:16:45 -0500

    signup form added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -78,6 +78,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :age, :location, :email, :password, :password_confirmation, :gender, :description)
+      params.require(:users).permit(:name, :age, :location, :email, :password, :password_confirmation, :gender, :description)
     end
 end
