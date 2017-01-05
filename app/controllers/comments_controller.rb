e705243fefed841b09288016b1524435bd80c30c
Samantha Guergenenov <samguergen@gmail.com>
2017-01-05 13:20:13 -0500

    storing furnitureid in new comm

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -73,6 +73,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:content).merge(user_id:session[:user_id], furniture_id: :furniture_id )
+      params.require(:comment).permit(:content, :furniture_id ).merge(user_id:session[:user_id])
     end
 end
