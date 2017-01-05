89e8d332ab15273648f4fc92e29c61096db7a503
Samantha Guergenenov <samguergen@gmail.com>
2017-01-05 13:51:59 -0500

    redirecting to relevant furni page after comm destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -56,9 +56,10 @@ class CommentsController < ApplicationController
   # DELETE /comments/1
   # DELETE /comments/1.json
   def destroy
+    @relevant_furni = @comment.furniture_id
     @comment.destroy
     respond_to do |format|
-      format.html { redirect_to comments_url, notice: 'Comment was successfully destroyed.' }
+      format.html { redirect_to furniture_path(@relevant_furni), notice: 'Comment was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
