8b6371f6858336c9b86d9baabb34c613b7f85f85
Samantha Guergenenov <samguergen@gmail.com>
2017-01-14 07:10:39 -0500

    now only one error page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,25 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def wishlist
+  	if current_user == nil
+  		redirect_to '/home/error'
+  	end  	
+  end
+
+  def settings
+  	if current_user == nil
+  		redirect_to '/home/error'
+  	end
+  end
+
+  def lottery
+  	if current_user == nil
+  		redirect_to '/home/error'
+  	end  	
+  end
+
+  def error
+  end
 end
