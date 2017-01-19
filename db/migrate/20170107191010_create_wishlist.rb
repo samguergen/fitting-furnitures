70d9d76e38dba4ee0abd1fa34630c8e5cfbc5522
Samantha Guergenenov <samguergen@gmail.com>
2017-01-19 06:55:59 -0500

    wishlist double mig removed

--- db/migrate/20170107191010_create_wishlist.rb
+++
@@ -1,8 +0,0 @@
-# class CreateWishlist < ActiveRecord::Migration
-#   def change
-#     create_table :wishlist, :id => false do |t|
-#       t.integer :furniture_id
-#       t.integer :user_id
-#     end
-#   end
-# end
