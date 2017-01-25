100b6cefcd2ab5928f01bb6441c1222fbb16a6dd
Samantha Guergenenov <samguergen@gmail.com>
2017-01-25 14:02:31 -0500

    fixed users to user but still not persisting image to db on user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,7 +27,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     puts 'image params are'
-    puts params[:users][:image]
+    puts params[:user][:image]
 
     respond_to do |format|
       if @user.save
